25d944b0a2ccee6cc3259ccf5d14669b66987eda
Rémi Eté <remi.ete@gmail.com>
2011-10-30 18:05:07 +0100

    modified

--- Test/testperl.pl
+++ Test/testperl.pl
@@ -8,3 +8,44 @@ $comp = sqrt(3);
 
 print ("par la racine : $len\npar le calcul : $comp\n");
 
+my @a;
+my $a;
+my @b;
+my $b;
+
+if (defined($a))
+{
+	print "scalar a : $a\n";
+}
+
+if ($a==0)
+{
+	print "scalar a : $a\n";
+}
+
+if (defined($b))
+{
+	print "scalar b : $b\n";
+}
+
+if (defined(@a))
+{
+	print "list a : @a\n";
+}
+
+if (defined(@b))
+{
+	print "list b : @b\n";
+}
+$a = 0;
+
+if (defined($a))
+{
+	print "(0) scalar a : $a\n";
+}
+
+if ($a==0)
+{
+	print "(a=0) scalar a : $a\n";
+}
+
